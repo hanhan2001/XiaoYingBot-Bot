dc6b3aaf5d9ee6649c5812c25fce81b7a2669691
Hanhan <764932129@qq.com>
2024-04-15 22:28:07 +0800

    fat: add new CommandSender which called

---
+++ src/main/java/me/xiaoying/bot/bot/command/Console.java
@@ -0,0 +1,13 @@
+package me.xiaoying.bot.bot.command;
+
+import me.xiaoying.bot.core.command.CommandSender;
+
+/**
+ * CommandSender console
+ */
+public class Console extends CommandSender {
+    @Override
+    public String toString() {
+        return "Console";
+    }
+}
